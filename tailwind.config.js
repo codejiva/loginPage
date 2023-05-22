2c559b0d4d2e9766ab25185a8f854fc403a64339
codejiva <rambatilumbungpardja@gmail.com>
2023-05-22 15:24:45 +0700

    Upload project loginPage_tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class', '[data-mode="dark"]'],
+  darkMode: 'class',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
